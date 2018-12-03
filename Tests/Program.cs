cedb966a84b150d0a0839a781ccb5dba87b3486b
jpb <jpb.06@outlook.com>
2018-12-03 21:09:54 +0100

    Entry point

---
+++ Tests/Program.cs
@@ -0,0 +1,27 @@
+﻿using DiscoDataRetriever.DataRetrieval;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FLDataRetriever fldr = new FLDataRetriever(@"D:\Games\Discovery_testdataextract\");
+                fldr.FetchData();
+
+                int ass = 0;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
